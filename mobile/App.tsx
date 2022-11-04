b5a937641ef694e72df79138b50dced9d4a6bf90
Gean Lucas <gean.lfpsa@gmail.com>
2022-11-04 12:33:23 -0300

    feat/mobile: add custom font

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,28 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-
 import { NativeBaseProvider, Center, Text } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { Loading } from './src/components/Loading';
+
+import { THEME } from './src/styles/theme';
+import { SignIn } from './src/screens/SignIn';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
+
   return (
-    <NativeBaseProvider>
-      <Center flex={1} bgColor="black">
-        <Text color="white" fontSize={24}>Hello React Native!</Text>
-        <StatusBar style="auto" />
+    <NativeBaseProvider theme={THEME}>
+      <Center flex={1} bgColor="gray.900">
+        { fontsLoaded ? <SignIn /> : <Loading /> }
       </Center>
     </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: '#fff',
-    fontSize: 24
-  }
-});
