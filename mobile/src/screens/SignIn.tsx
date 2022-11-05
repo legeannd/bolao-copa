ed6401beb5906933b9a8ddc69e199f7fc402af33
Gean Lucas <gean.lfpsa@gmail.com>
2022-11-05 00:50:38 -0300

    feat/mobile: auth with google

--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -17,6 +17,7 @@ export function SignIn() {
         title="Entrar com Google"
         leftIcon={<Icon as={Fontisto} name="google" color="white" size="md"/>}
         mt={12}
+        onPress={signIn}
       />
 
       <Text color="white" textAlign="center" mt={4}>
