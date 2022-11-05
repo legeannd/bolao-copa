f4dc563152cfcc57837891bd2431c15e29f6eb98
Gean Lucas <gean.lfpsa@gmail.com>
2022-11-05 17:49:55 -0300

    feat/server: create profile route

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -2,8 +2,15 @@ import { FastifyInstance } from "fastify"
 import fetch from "node-fetch"
 import { z } from "zod"
 import { prisma } from "../lib/prisma"
+import { authenticate } from "../plugins/authenticate"
 
 export async function authRoutes(fastify: FastifyInstance) {
+  fastify.get('/me', {
+      onRequest: [authenticate],
+    }, async (request) => {
+    return { user: request.user }
+  })
+  
   fastify.post('/users', async (request) => {
     const createUserBody = z.object({
       access_token: z.string(),
