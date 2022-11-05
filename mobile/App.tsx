4c6c2501e4d7f6345175bfda4783e358c8847a76
Gean Lucas <gean.lfpsa@gmail.com>
2022-11-05 01:07:58 -0300

    feat/mobile: create find pool by code screen

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,6 +7,7 @@ import { THEME } from './src/styles/theme';
 import { SignIn } from './src/screens/SignIn';
 import { AuthContextProvider } from './src/contexts/AuthContext';
 import { New } from './src/screens/New';
+import { Find } from './src/screens/Find';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold });
@@ -19,7 +20,7 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        { fontsLoaded ? <New /> : <Loading /> }
+        { fontsLoaded ? <Find /> : <Loading /> }
       </AuthContextProvider>
     </NativeBaseProvider>
   );
