ab4b2a294ef61979942a124510306c7dca5d6ea0
Gean Lucas <gean.lfpsa@gmail.com>
2022-11-06 00:07:03 -0300

    feat/mobile: create polls flatlist

--- mobile/src/screens/Polls.tsx
+++ mobile/src/screens/Polls.tsx
@@ -1,12 +1,44 @@
-import { VStack, Icon } from "native-base";
+import { useEffect, useState } from "react";
+import { VStack, Icon, useToast, FlatList } from "native-base";
 import { Octicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native'; 
 
 import { Button } from "../components/Button";
 import { Header } from "../components/Header";
+import { api } from "../services/api";
+import { Loading } from "../components/Loading";
+import { PoolCard, PoolCardProps } from "../components/PoolCard";
+import { EmptyPoolList } from "../components/EmptyPoolList";
 
 export function Polls() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [polls, setPolls] = useState<PoolCardProps[]>([])
   const { navigate } = useNavigation();
+
+  const toast = useToast()
+  
+  async function fetchPolls() {
+    try {
+      setIsLoading(true)
+      const response = await api.get('/polls')
+      setPolls(response.data.polls)
+
+    } catch (error) {
+      console.log(error)
+
+      toast.show({
+        title: 'Não foi possível carregar os bolões',
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchPolls()
+  }, [])
 
   return (
     <VStack flex={1} bgColor="gray.900">
@@ -18,6 +50,18 @@ export function Polls() {
           onPress={() => navigate('find')}
         />  
       </VStack>
+
+      {isLoading ? <Loading /> : (
+        <FlatList 
+          data={polls}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => <PoolCard data={item} />}
+          showsVerticalScrollIndicator={false}
+          ListEmptyComponent={() => <EmptyPoolList />}
+          _contentContainerStyle={{ pb: 10}}
+          px={5}
+        />
+      )}
     </VStack>
   )
 }
